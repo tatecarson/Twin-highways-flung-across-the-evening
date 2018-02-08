874997f6132cdefcdb5cb8a5bef8f94ea75909f7
Tate Carson <tatecarson@Jessicas-MacBook-Pro.local>
2018-02-07 22:33:03 -0600

    first commit

---
+++ start.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+open /Applications/Wekinator.app &
+/Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang mainInit.scd 
+
+
+# sleep 1
+
+# killall scsynth
+
+# sleep 1
+
+# done
